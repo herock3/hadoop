560290fe553bcb4658dd30eb1873ab3f784cd7ab
CISCO\yingxioz <yingxioz@cisco.com>
2017-11-29 10:24:30 +0800

    2017/11/27

--- src/main/java/cn/zyx/hadoop/rpc/client/MyHdfsClient.java
+++ src/main/java/cn/zyx/hadoop/rpc/client/MyHdfsClient.java
@@ -10,7 +10,7 @@ import cn.zyx.hadoop.rpc.protocol.ClientNameNodeProtocol;
 public class MyHdfsClient {
 
 	public static void main(String[] args) throws Exception {
-		ClientNameNodeProtocol namenode = RPC.getProxy(ClientNameNodeProtocol.class, 1L,
+		ClientNameNodeProtocol namenode =(ClientNameNodeProtocol) RPC.getProxy(ClientNameNodeProtocol.class, 1L,
 				new InetSocketAddress("localhost", 8888), new Configuration());
 		String metaData = namenode.getMetaData("/angela.mygirl");
 		System.out.println(metaData);
--- src/main/java/cn/zyx/hadoop/rpc/service/PublishServiceUtil.java
+++ src/main/java/cn/zyx/hadoop/rpc/service/PublishServiceUtil.java
@@ -1,18 +1,46 @@
 package cn.zyx.hadoop.rpc.service;
 import cn.zyx.hadoop.rpc.protocol.ClientNameNodeProtocol;
+import cn.zyx.hadoop.rpc.protocol.IuserLoginService;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.ipc.RPC;
 import org.apache.hadoop.ipc.RPC.Builder;
+import org.junit.Before;
+import org.junit.Test;
+import org.omg.CORBA.INITIALIZE;
+
 import java.io.IOException;
 
 public class PublishServiceUtil {
-    public static void main(String[] args) throws IOException {
-        Builder builder = new Builder(new Configuration());
-        builder.setBindAddress("localhost")
-                .setPort(8888)
+    private static Builder builder=null;
+    private static final String ADDRESS="localhost";
+
+    public static void init(){
+      builder = new Builder(new Configuration());
+      builder.setBindAddress(ADDRESS);
+    }
+
+
+    public static void nameNodeProtocol() throws IOException {
+        builder.setPort(8888)
                 .setProtocol(ClientNameNodeProtocol.class)
                 .setInstance(new MyNameNode());
-        RPC.Server server= builder.build();
+        RPC.Server server = builder.build();
         server.start();
     }
+
+
+    public static void userLoginService() throws IOException {
+        builder.setPort(9999)
+                .setProtocol(IuserLoginService.class)
+                .setInstance(new UserLoginServiceImpl());
+        RPC.Server server = builder.build();
+        server.start();
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        nameNodeProtocol();
+        userLoginService();
+    }
+
 }
